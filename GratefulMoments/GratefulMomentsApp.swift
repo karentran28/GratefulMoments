bb2d7537ba7d7dcfb4b6192828a63c6b0f6b59af
Karen Tran <karentran28@hotmail.com>
2025-12-24 13:25:17 -0800

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Karen Tran on 2025-12-24.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
