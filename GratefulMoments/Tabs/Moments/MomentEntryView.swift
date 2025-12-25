60a4f6c5874cae31b81e7bfc13842f76bb1c221f
Karen Tran <karentran28@hotmail.com>
2025-12-24 17:51:46 -0800

    initial commit

---
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -0,0 +1,74 @@
+//
+//  MomentEntryView.swift
+//  GratefulMoments
+//
+//  Created by Karen Tran on 2025-12-24.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct MomentEntryView: View {
+    @State private var title = ""
+    @State private var note = ""
+    @State private var imageData: Data?
+    @State private var newImage: PhotosPickerItem?
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                contentStack
+            }
+            .scrollDismissesKeyboard(.interactively)
+            .navigationTitle("Grateful for")
+        }
+    }
+    
+    private var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group {
+                if let imageData, let uIImage = UIImage(data: imageData) {
+                    Image(uiImage: uIImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: newImage) {
+            guard let newImage else { return }
+            Task {
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
+        }
+    }
+    
+    var contentStack: some View {
+        VStack(alignment: .leading) {
+            TextField(text: $title) {
+                Text("Title (Required)")
+            }
+            .font(.title.bold())
+            .padding(.top, 48)
+            Divider()
+            
+            TextField("Log your small wins", text: $note, axis: .vertical)
+                .multilineTextAlignment(.leading)
+                .lineLimit(5...Int.max)
+            
+            photoPicker
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MomentEntryView()
+}
+
